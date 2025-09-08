253d4c077c4cb983a1fa527ccc7b38cfc057d40c
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2025-09-08 11:01:37 -0400

    move img between title and desc

--- components/Project.js
+++ components/Project.js
@@ -5,19 +5,33 @@ export default function Project({ project }) {
 
   return (
     <div
-      className={`flex flex-col ${
-        isEven ? "md:flex-row" : "md:flex-row-reverse"
+      className={`flex flex-col md:flex-row ${
+        isEven ? "" : "md:flex-row-reverse"
       } my-8 py-8`}
     >
-      {/* Text content */}
-      <div className="md:basis-1/3 mt-4 md:mt-8 px-4 md:px-8 flex flex-col">
-        <div className="text-2xl font-semibold mt-4 md:mt-8">
+      {/* Left/Right column on desktop */}
+      <div className="md:basis-2/3 flex flex-col">
+        {/* Title */}
+        <div className="text-2xl font-semibold mt-4 md:mt-0 px-4 md:px-0">
           {project.name}
         </div>
-        <div className="my-4 md:my-6 text-sm md:text-base">
+
+        {/* Image */}
+        <div className="mt-4 md:mt-4 px-4 md:px-0 transition duration-300 hover:scale-95">
+          <img
+            src={`${basePath}/assets/img/${project.id}.${project.img_file_type}`}
+            alt={project.name}
+            className="w-full max-h-[500px] rounded object-cover"
+          />
+        </div>
+
+        {/* Description */}
+        <div className="my-4 md:my-6 px-4 md:px-0 text-sm md:text-base">
           {project.description}
         </div>
-        <div className="my-4 md:my-6 font-semibold transition duration-500">
+
+        {/* Links */}
+        <div className="flex flex-row gap-4 px-4 md:px-0 my-4 md:my-6 font-semibold transition duration-500">
           <a
             href={project.demo_url}
             target="_blank"
@@ -25,8 +39,6 @@ export default function Project({ project }) {
           >
             Live Demo
           </a>
-        </div>
-        <div className="mb-4 md:mb-6 font-semibold transition duration-500">
           <a
             href={project.github_url}
             target="_blank"
@@ -35,15 +47,6 @@ export default function Project({ project }) {
             Learn More
           </a>
         </div>
-      </div>
-
-      {/* Image */}
-      <div className="md:basis-2/3 mt-4 md:mt-0 transition duration-300 hover:scale-95">
-        <img
-          src={`${basePath}/assets/img/${project.id}.${project.img_file_type}`}
-          alt={project.name}
-          className="w-full max-h-[500px] rounded object-cover"
-        />
       </div>
     </div>
   );
